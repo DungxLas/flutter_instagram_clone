6d2be1366ac11ea256a6a65a8c165416c1399c31
DungxLas <85223326+DungxLas@users.noreply.github.com>
2023-12-28 21:36:18 +0700

    ....

---
+++ lib/resources/firestore_methods.dart
@@ -0,0 +1,35 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_instagram_clone/models/post.dart';
+import 'package:flutter_instagram_clone/resources/storage_methods.dart';
+import 'package:uuid/uuid.dart';
+
+class FireStoreMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<String> uploadPost(String caption, Uint8List file, String uid,
+      String username, String profImage) async {
+    String res = "Some error occurred";
+    try {
+      String photoUrl =
+          await StorageMethods().uploadImageToStorage('post', file, true);
+      String postId = const Uuid().v1(); // creates unique id based on time
+      Post post = Post(
+        description: caption,
+        uid: uid,
+        username: username,
+        likes: [],
+        postId: postId,
+        datePublished: DateTime.now(),
+        postUrl: photoUrl,
+        profImage: profImage,
+      );
+      _firestore.collection('posts').doc(postId).set(post.toJson());
+      res = "success";
+    } catch (e) {
+      res = e.toString();
+    }
+    return res;
+  }
+}
